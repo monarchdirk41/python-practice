b2584b9a03776d935771b59f10d9966c7e22c6dc
James Maverick Parenas <monarchdirk41@gmail.com>
2025-05-02 22:25:16 +0800

    Update main.py

--- age_checker/main.py
+++ age_checker/main.py
@@ -1,22 +1,31 @@
+max_attempts = 3
+attempts = 0
+
 
 def input_age():
     try:
-        enter_age = int(input("Enter your age: "))
-        if enter_age <= 0:
-            print("Age must be greater than 0.")
-        else:
-            print(f"Thank You! You entered {enter_age}")
+       enter_age = int(input("Enter your age: "))
+       if enter_age < 18:
+            print("Sorry, You must be 18+ to continue.")
+       else:
+            print(f"Thank You! Your age is {enter_age}. Proceeding...")
             return True
 
     except ValueError:
         print("Please enter a valid number.")
+
     return False
 
 
 
-while True:
+while attempts < max_attempts:
    if input_age():
        break
+   attempts = attempts + 1
+   if attempts < max_attempts:
+       print(f"Attempts left: {max_attempts - attempts}")
+   else:
+       print("Maximum attempts reached. Exiting")
 
 
 
